a2c1b00a0a3344b22c726a57c434a440058c1fd1
Ana-Luisa-Rigotti <alohapovo@gmail.com>
2026-03-27 22:12:45 -0300

    Update build.gradle.kts

--- ifood_linear/app/build.gradle.kts
+++ ifood_linear/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
 
     defaultConfig {
         applicationId = "com.example.ifood_linear"
-        minSdk = 36
-        targetSdk = 36
+        minSdk = 26
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
